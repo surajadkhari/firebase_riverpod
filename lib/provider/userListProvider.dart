f04186f0a83c9144217eeb9757a9c365948010b2
surajadkhari <surajadkhari@gmail.com>
2021-10-21 15:26:54 +0545

    set state from emplty to userdataList

--- lib/provider/userListProvider.dart
+++ lib/provider/userListProvider.dart
@@ -1,42 +1,28 @@
 import 'dart:convert';
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_riverpod/models/usermodel.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart';
 
-class UserNotifier extends StateNotifier<List<User>> {
+class UserNotifier extends StateNotifier<List<Users>> {
   UserNotifier() : super([]);
 
   loadUsers() async {
-    List<User> userdataList = [];
+    List<Users> userdataList = [];
     var endPoint = 'https://reqres.in/api/users?page=2';
     var respond = await get(Uri.parse(endPoint));
     var jsonData = jsonDecode(respond.body)['data'];
 
     //For each--Mapping
     for (int index = 0; index < jsonData.lenght; index++) {
-      userdataList.add(User(
+      userdataList.add(Users(
           id: jsonData[index]['id'],
           email: jsonData[index]['email'],
           first_name: jsonData[index]['first_name'],
           last_name: jsonData[index]['last_name'],
           avatar: jsonData[index]['avatar']));
     }
+    state = userdataList;
   }
-}
-
-//Model
-
-class User {
-  final int id;
-  final String email;
-  final String first_name;
-  final String last_name;
-  final String avatar;
-  User(
-      {required this.id,
-      required this.email,
-      required this.first_name,
-      required this.last_name,
-      required this.avatar});
 }
