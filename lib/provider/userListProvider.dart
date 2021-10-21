ac212dc4bdce3ccaa7d48e0bc8e2cdf7be6bf087
surajadkhari <surajadkhari@gmail.com>
2021-10-21 14:44:21 +0545

    Create userListProvider , Usenotifier as State Notifier and create User Model for Json

---
+++ lib/provider/userListProvider.dart
@@ -0,0 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class UserNotifier extends StateNotifier<List<User>> {
+  UserNotifier(state) : super(state);
+}
+
+class User {
+  final int id;
+  final String email;
+  final String first_name;
+  final String last_name;
+  final String avatar;
+  User(
+      {required this.id,
+      required this.email,
+      required this.first_name,
+      required this.last_name,
+      required this.avatar});
+}
