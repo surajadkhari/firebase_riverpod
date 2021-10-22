fe5645d4b29246e08cc98385535511e46c1d89cc
surajadkhari <surajadkhari@gmail.com>
2021-10-22 16:23:41 +0545

    issue solved

--- lib/models/post_feed_model.dart
+++ lib/models/post_feed_model.dart
@@ -9,9 +9,9 @@ class PostFeed {
 
   PostFeed.fromJson(QueryDocumentSnapshot data) {
     this.title = data['title'] ?? " There is no title";
-    this.description = data['description'] == null
+    this.description = data['decription'] == null
         ? "There is no description"
-        : data['description'];
+        : data['decription'];
     this.image = data['image'] ??
         "https://www.name.edu.np/wp-content/uploads/2020/06/placeholder.png";
   }
